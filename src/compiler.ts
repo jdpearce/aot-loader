b96560c7edad39868bec91436f68a83e5e993f54
Ryan Clark <ryan@ryanclark.me>
2017-02-23 03:32:39 +0000

    chore(compiler): formatting

--- src/compiler.ts
+++ src/compiler.ts
@@ -2,17 +2,19 @@ import { CompilerHost, CompilerHostContext } from '@angular/compiler-cli';
 import { CompilerOptions, Program, SourceFile } from 'typescript';
 
 export interface AngularCompilerOptions extends CompilerOptions {
-  basePath?: string;
-  genDir?: string;
-  entryModule?: string;
-  debug?: boolean;
+  basePath?: string,
+  genDir?: string,
+  entryModule?: string,
+  debug?: boolean
 }
 
 export class AotCompilerHost extends CompilerHost {
-  constructor(protected program: Program,
-              protected options: AngularCompilerOptions,
-              protected context: CompilerHostContext,
-              protected sourceFileCache: Map<string, SourceFile>) {
+  constructor(
+    protected program: Program,
+    protected options: AngularCompilerOptions,
+    protected context: CompilerHostContext,
+    protected sourceFileCache: Map<string, SourceFile>
+  ) {
     super(program, options, context);
   }
 
